d7514c9617d07f6ec0ff6229e9994a9ea473485c
maximilianjones <maxcharles500@gmail.com>
2021-11-22 16:23:00 -0800

    commit tree 95c6cf6e35ae00fb297a775c0f4f2dfb4a16a6a6

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+ if (someValue <= 400) {
+   return 'This one is on me!'
+ }
+ else if (someValue > 2000 && someValue <= 2500) {
+   return 'I will gladly take your thirty bucks.'
+ }
+ else if (someValue > 2500){
+   return 'No can do.'
+ }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityString){
   // Write your code here!
+return (cityString === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipValue){
   // Write your code here!
+switch(tipValue) {
+  case 'generous':
+    return "Thank you so much.";
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.'
+  }
 }
